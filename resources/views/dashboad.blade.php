2ec83dede7830b44d798e9a26486ed72c899eb82
sadiqeen <finixlllsarawut@gmail.com>
2020-08-10 17:48:22 +0700

    initial project

---
+++ resources/views/dashboad.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+
+        <div class="col-md-6">
+            <div class="card">
+                <h5 class="card-header">{{ __('Sales summary') }}</h5>
+                <div class="card-body">
+                    <sale-graph :chartdata="props.chart1.data" :options="props.chart1.options" />
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <h5 class="card-header">{{ __('Supplier summary') }}</h5>
+                <div class="card-body">
+                    <sale-graph :chartdata="props.chart2.data" :options="props.chart2.options" />
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
+
+@push('script')
+<script>
+    const chart1 = {
+        data: {
+            labels: ['one', 'two', 'three', 'four', 'five'],
+            datasets: [{
+                label: 'Sales',
+                backgroundColor: '#f87979',
+                data: [40, 20, 23, 15, 10]
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    }
+
+    const chart2 = {
+        data: {
+            labels: ['Makro', 'BigC', 'CP', 'K\'Nah'],
+            datasets: [{
+                label: 'Supplier',
+                backgroundColor: '#f87979',
+                data: [5, 10, 15, 20, 25]
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    }
+
+    const props = {
+        chart1: chart1,
+        chart2: chart2,
+    }
+
+</script>
+@endpush
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <!-- CSRF Token -->
+    <meta content="{{ csrf_token() }}" name="csrf-token">
+    <title>
+        {{ config('app.name', 'Laravel') }}
+    </title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        {{-- Top bar --}}
+        <nav class="navbar navbar-expand-sm navbar-light bg-white shadow d-none d-sm-none d-md-block">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                    class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
+                    <span class="navbar-toggler-icon">
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">
+                                {{ __('Login') }}
+                            </a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">
+                                {{ __('Register') }}
+                            </a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle"
+                                data-toggle="dropdown" href="#" id="navbarDropdown" role="button" v-pre="">
+                                {{ Auth::user()->name }}
+                                <span class="caret">
+                                </span>
+                            </a>
+                            <div aria-labelledby="navbarDropdown" class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form action="{{ route('logout') }}" id="logout-form" method="POST"
+                                    style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        {{-- manu --}}
+        @auth
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow" id="navBar">
+            <div class="container">
+                <a class="navbar-brand d-sm-block d-md-none" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button aria-controls="menuArea" aria-expanded="false" aria-label="Toggle navigation"
+                    class="navbar-toggler" data-target="#menuArea" data-toggle="collapse" type="button">
+                    <span class="navbar-toggler-icon">
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="menuArea">
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Orders
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle"
+                                data-toggle="dropdown" href="#" id="productDropdown" role="button">
+                                Product
+                            </a>
+                            <div aria-labelledby="productDropdown" class="dropdown-menu">
+                                <a class="dropdown-item" href="#">
+                                    Action
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    Category
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Customer
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Supplier
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle"
+                                data-toggle="dropdown" href="#" id="settingDropdown" role="button">
+                                Setting
+                            </a>
+                            <div aria-labelledby="settingDropdown" class="dropdown-menu">
+                                <a class="dropdown-item" href="#">
+                                    Action
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    Another action
+                                </a>
+                                <div class="dropdown-divider">
+                                </div>
+                                <a class="dropdown-item" href="#">
+                                    Something else here
+                                </a>
+                            </div>
+                        </li>
+
+                        @guest
+                        <li class="nav-item d-sm-block d-md-none">
+                            <a class="nav-link" href="{{ route('login') }}">
+                                {{ __('Login') }}
+                            </a>
+                        </li>
+                        @else
+                        <li class="nav-item dropdown d-sm-block d-md-none">
+                            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle"
+                                data-toggle="dropdown" href="#" id="navbarDropdown" role="button" v-pre="">
+                                {{ Auth::user()->name }}
+                                <span class="caret">
+                                </span>
+                            </a>
+                            <div aria-labelledby="navbarDropdown" class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        @endauth
+        <main class="py-4" id="app">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- Scripts -->
+    <script defer="" src="{{ asset('js/app.js') }}"></script>
+    @stack('script')
+
+</body>
+
+</html>
