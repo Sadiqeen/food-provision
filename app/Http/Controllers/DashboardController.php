66470d04232154f1e5ce7e41376f6374b1e042ee
sadiqeen <finixlllsarawut@gmail.com>
2020-08-13 11:25:22 +0700

    [Edit] move dashboard view into admin folder

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
      */
     public function __construct()
     {
-    	// 
+    	//
     }
 
     /**
@@ -23,6 +23,6 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboad');
+        return view('admin.dashboad');
     }
 }
