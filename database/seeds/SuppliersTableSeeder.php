e35e4f25f1fceb55b1ceb3f0c168ab0cfed603f5
sadiqeen <finixlllsarawut@gmail.com>
2020-08-14 00:28:35 +0700

    [Edit] Add seeder data

---
+++ database/seeds/SuppliersTableSeeder.php
@@ -0,0 +1,354 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SuppliersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        \DB::table('suppliers')->delete();
+
+        \DB::table('suppliers')->insert(array (
+            0 =>
+            array (
+                'id' => 1,
+                'name' => 'Kunde, Keebler and Jerde',
+                'tel' => '800-421-8066',
+                'email' => 'kbartoletti@hotmail.com',
+                'address' => '58764 Stamm Row
+West Name, IL 75271-7513',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            1 =>
+            array (
+                'id' => 2,
+                'name' => 'Crona, Bosco and Medhurst',
+                'tel' => '855-584-5894',
+                'email' => 'golden.kuphal@yahoo.com',
+                'address' => '2155 Damien Trace
+Deechester, NE 70134-2145',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            2 =>
+            array (
+                'id' => 3,
+                'name' => 'Moore, Haley and Hand',
+                'tel' => '844-734-7688',
+                'email' => 'umertz@gmail.com',
+                'address' => '81789 Garland Street
+New Lacy, AR 47972',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            3 =>
+            array (
+                'id' => 4,
+                'name' => 'Jacobson-Lueilwitz',
+            'tel' => '(844) 852-7634',
+                'email' => 'lehner.nyasia@yahoo.com',
+                'address' => '473 Icie Hills Suite 325
+North Eloisaberg, HI 56452-3164',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            4 =>
+            array (
+                'id' => 5,
+                'name' => 'Schmidt, Will and Ledner',
+            'tel' => '(866) 527-9392',
+                'email' => 'imogene.altenwerth@hotmail.com',
+                'address' => '401 Antonia Plaza
+Lake Isaiahburgh, AK 29734',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            5 =>
+            array (
+                'id' => 6,
+                'name' => 'Frami-Bernhard',
+                'tel' => '844-320-5503',
+                'email' => 'corkery.helga@hotmail.com',
+                'address' => '8803 Betsy Stravenue
+North Trent, NY 30028',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            6 =>
+            array (
+                'id' => 7,
+                'name' => 'Haag-Kling',
+                'tel' => '888.717.3569',
+                'email' => 'keichmann@gmail.com',
+                'address' => '746 Feest Mount
+East Lelah, ID 88861-3647',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            7 =>
+            array (
+                'id' => 8,
+                'name' => 'Orn-Braun',
+                'tel' => '877-861-1752',
+                'email' => 'etoy@gmail.com',
+                'address' => '53830 Jensen Brook Suite 453
+Port Harry, VT 27573',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            8 =>
+            array (
+                'id' => 9,
+                'name' => 'Franecki, Muller and McCullough',
+                'tel' => '800-967-9698',
+                'email' => 'norberto63@gmail.com',
+                'address' => '768 Kaleigh Coves Suite 702
+North Lura, MS 65105-5304',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            9 =>
+            array (
+                'id' => 10,
+                'name' => 'Spencer-McCullough',
+                'tel' => '844-917-7202',
+                'email' => 'kaci.gibson@gmail.com',
+                'address' => '90846 Chanelle Fall
+Lake Kathlyn, CA 58928',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            10 =>
+            array (
+                'id' => 11,
+                'name' => 'Haley, Bartoletti and Mante',
+                'tel' => '844-375-8854',
+                'email' => 'claire.kuhic@yahoo.com',
+                'address' => '57512 Pasquale Ranch
+North Al, WV 82345',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            11 =>
+            array (
+                'id' => 12,
+                'name' => 'Weissnat Group',
+            'tel' => '(855) 287-2120',
+                'email' => 'lue11@yahoo.com',
+                'address' => '87055 Morissette Mountain Apt. 143
+New Eldredberg, MN 80773',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            12 =>
+            array (
+                'id' => 13,
+                'name' => 'Fadel LLC',
+            'tel' => '(855) 593-8769',
+                'email' => 'marquis.schiller@gmail.com',
+                'address' => '3396 Schoen Corners
+Paxtontown, TX 54797',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            13 =>
+            array (
+                'id' => 14,
+                'name' => 'Cummings, Zemlak and Keeling',
+                'tel' => '1-888-893-4262',
+                'email' => 'joel91@yahoo.com',
+                'address' => '7383 Isidro Court Apt. 981
+Wisokychester, ND 95548-3573',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            14 =>
+            array (
+                'id' => 15,
+                'name' => 'Friesen, Cartwright and Bechtelar',
+                'tel' => '866-231-4350',
+                'email' => 'jordy.batz@hotmail.com',
+                'address' => '7851 Isidro Station
+Nelliechester, MA 29597-7885',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            15 =>
+            array (
+                'id' => 16,
+                'name' => 'Treutel-Hegmann',
+            'tel' => '(855) 425-1073',
+                'email' => 'faye.effertz@hotmail.com',
+                'address' => '2777 Heaney Passage
+New Adriannafort, SC 28744',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            16 =>
+            array (
+                'id' => 17,
+                'name' => 'Moen LLC',
+            'tel' => '(800) 517-0838',
+                'email' => 'tmann@gmail.com',
+                'address' => '8296 Crooks Manor Apt. 201
+Port Max, SD 56546',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            17 =>
+            array (
+                'id' => 18,
+                'name' => 'Schaefer LLC',
+            'tel' => '(855) 427-2202',
+                'email' => 'simone91@hotmail.com',
+                'address' => '6870 Hertha Ferry Suite 706
+Port Luciusmouth, VT 06442-1967',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            18 =>
+            array (
+                'id' => 19,
+                'name' => 'Schamberger, Mueller and Stokes',
+                'tel' => '1-877-846-7880',
+                'email' => 'balistreri.zander@hotmail.com',
+                'address' => '597 Wilderman Crossing Suite 003
+West Miracle, CO 65283-0077',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            19 =>
+            array (
+                'id' => 20,
+                'name' => 'Simonis, Balistreri and Ledner',
+            'tel' => '(855) 318-5737',
+                'email' => 'legros.beaulah@yahoo.com',
+                'address' => '21914 Ross Locks Suite 501
+Auertown, DC 03829-6889',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            20 =>
+            array (
+                'id' => 21,
+                'name' => 'Kub-Rau',
+                'tel' => '855-746-6092',
+                'email' => 'abigale81@gmail.com',
+                'address' => '7553 Kirlin Corner
+South Terrillshire, PA 87290-1656',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            21 =>
+            array (
+                'id' => 22,
+                'name' => 'Rippin PLC',
+                'tel' => '855.624.9193',
+                'email' => 'oren07@gmail.com',
+                'address' => '371 Kilback Keys Apt. 728
+North Teagan, AK 41093-9132',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            22 =>
+            array (
+                'id' => 23,
+                'name' => 'Pfannerstill-Bruen',
+                'tel' => '1-877-360-8715',
+                'email' => 'adan39@yahoo.com',
+                'address' => '85850 Roob Prairie
+North Joshland, ND 24505-4128',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            23 =>
+            array (
+                'id' => 24,
+                'name' => 'Littel, Beatty and Swift',
+                'tel' => '800-407-1116',
+                'email' => 'uschulist@yahoo.com',
+                'address' => '8451 Deja Run Apt. 096
+South Avisport, GA 57672',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            24 =>
+            array (
+                'id' => 25,
+                'name' => 'O\'Connell-Glover',
+                'tel' => '888-267-4391',
+                'email' => 'auer.brandon@yahoo.com',
+                'address' => '29715 Matilde Land
+Spencerland, ID 19944',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            25 =>
+            array (
+                'id' => 26,
+                'name' => 'Schulist, Ruecker and Franecki',
+                'tel' => '1-800-420-7492',
+                'email' => 'zschuppe@yahoo.com',
+                'address' => '58942 Diana Crossroad
+Angelamouth, MI 60646',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            26 =>
+            array (
+                'id' => 27,
+                'name' => 'Feeney, Osinski and Jenkins',
+                'tel' => '1-844-387-3900',
+                'email' => 'johann.schaden@hotmail.com',
+                'address' => '9784 Cormier Lake Suite 327
+Lake Mekhi, ID 45073',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            27 =>
+            array (
+                'id' => 28,
+                'name' => 'Dietrich LLC',
+                'tel' => '877.448.3790',
+                'email' => 'xkoepp@hotmail.com',
+                'address' => '682 Tessie Prairie Apt. 227
+Merrittview, SC 38251-0046',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            28 =>
+            array (
+                'id' => 29,
+                'name' => 'Hyatt, Carroll and Ferry',
+            'tel' => '(855) 317-6488',
+                'email' => 'auer.nicklaus@gmail.com',
+                'address' => '26620 Mraz Forge
+Lake Shanelburgh, MO 85310-4895',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+            29 =>
+            array (
+                'id' => 30,
+                'name' => 'Mraz, Sanford and Kshlerin',
+                'tel' => '1-800-669-4684',
+                'email' => 'dkonopelski@hotmail.com',
+                'address' => '986 Martine Spur Apt. 987
+Matildeberg, ID 54683-4614',
+                'created_at' => '2020-08-13 17:25:57',
+                'updated_at' => '2020-08-13 17:25:57',
+            ),
+        ));
+
+
+    }
+}
