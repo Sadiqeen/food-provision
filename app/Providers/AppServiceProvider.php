d1fa4d89e7eb59d4f2add448c607d8ad45884b48
sadiqeen <finixlllsarawut@gmail.com>
2020-08-14 11:34:03 +0700

    add condition fource https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app["request"]->server->set('HTTPS', true);
+        if (env('APP_ENV') === 'production') {
+            $this->app["request"]->server->set('HTTPS', true);
+        }
     }
 
     /**
