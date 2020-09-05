5ad501419715dd651f72bd057838067161a8fecd
sadiqeen <finixlllsarawut@gmail.com>
2020-09-05 20:02:37 +0700

    [Edited] change seeder to real data

--- database/seeds/BrandsTableSeeder.php
+++ database/seeds/BrandsTableSeeder.php
@@ -17,80 +17,32 @@ class BrandsTableSeeder extends Seeder
             0 =>
             array (
                 'id' => 1,
-                'name_en' => 'Garment',
-                'name_th' => 'ไทย_Gas Pumping Station Operator',
+                'name_en' => 'Local',
+                'name_th' => 'ท้องถิ่น',
                 'created_at' => '2020-09-02 19:45:28',
                 'updated_at' => '2020-09-02 19:45:28',
             ),
             1 =>
             array (
                 'id' => 2,
-                'name_en' => 'Film Laboratory Technician',
-                'name_th' => 'ไทย_Welding Machine Tender',
+                'name_en' => 'Imported',
+                'name_th' => 'นำเข้า',
                 'created_at' => '2020-09-02 19:45:28',
                 'updated_at' => '2020-09-02 19:45:28',
             ),
             2 =>
             array (
                 'id' => 3,
-                'name_en' => 'Distribution Manager',
-                'name_th' => 'ไทย_Horticultural Worker',
+                'name_en' => 'Sai nam pung',
+                'name_th' => 'สายน้ำผึ้ง',
                 'created_at' => '2020-09-02 19:45:28',
                 'updated_at' => '2020-09-02 19:45:28',
             ),
             3 =>
             array (
                 'id' => 4,
-                'name_en' => 'Custom Tailor',
-                'name_th' => 'ไทย_Model Maker',
-                'created_at' => '2020-09-02 19:45:28',
-                'updated_at' => '2020-09-02 19:45:28',
-            ),
-            4 =>
-            array (
-                'id' => 5,
-                'name_en' => 'Announcer',
-                'name_th' => 'ไทย_Cutting Machine Operator',
-                'created_at' => '2020-09-02 19:45:28',
-                'updated_at' => '2020-09-02 19:45:28',
-            ),
-            5 =>
-            array (
-                'id' => 6,
-                'name_en' => 'Makeup Artists',
-                'name_th' => 'ไทย_Gluing Machine Operator',
-                'created_at' => '2020-09-02 19:45:28',
-                'updated_at' => '2020-09-02 19:45:28',
-            ),
-            6 =>
-            array (
-                'id' => 7,
-                'name_en' => 'Economist',
-                'name_th' => 'ไทย_Entertainment Attendant',
-                'created_at' => '2020-09-02 19:45:28',
-                'updated_at' => '2020-09-02 19:45:28',
-            ),
-            7 =>
-            array (
-                'id' => 8,
-                'name_en' => 'Food Science Technician',
-                'name_th' => 'ไทย_Operations Research Analyst',
-                'created_at' => '2020-09-02 19:45:28',
-                'updated_at' => '2020-09-02 19:45:28',
-            ),
-            8 =>
-            array (
-                'id' => 9,
-                'name_en' => 'Sheet Metal Worker',
-                'name_th' => 'ไทย_Statistician',
-                'created_at' => '2020-09-02 19:45:28',
-                'updated_at' => '2020-09-02 19:45:28',
-            ),
-            9 =>
-            array (
-                'id' => 10,
-                'name_en' => 'Punching Machine Setters',
-                'name_th' => 'ไทย_Forging Machine Setter',
+                'name_en' => 'Meiji',
+                'name_th' => 'เมจิ',
                 'created_at' => '2020-09-02 19:45:28',
                 'updated_at' => '2020-09-02 19:45:28',
             ),
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -17,160 +17,32 @@ class CategoriesTableSeeder extends Seeder
             0 =>
             array (
                 'id' => 1,
-                'name_en' => 'Concierge',
-                'name_th' => 'ประเภทสินค้า_Product Management Leader',
+                'name_en' => 'Chiller',
+                'name_th' => 'แช่เย็น',
                 'created_at' => '2020-09-03 07:20:32',
                 'updated_at' => '2020-09-03 07:20:32',
             ),
             1 =>
             array (
                 'id' => 2,
-                'name_en' => 'Airfield Operations Specialist',
-                'name_th' => 'ประเภทสินค้า_Gaming Dealer',
+                'name_en' => 'Freezer',
+                'name_th' => 'แช่แข็ง',
                 'created_at' => '2020-09-03 07:20:32',
                 'updated_at' => '2020-09-03 07:20:32',
             ),
             2 =>
             array (
                 'id' => 3,
-                'name_en' => 'Welder',
-                'name_th' => 'ประเภทสินค้า_Machine Feeder',
+                'name_en' => 'Dry',
+                'name_th' => 'ของแห้ง',
                 'created_at' => '2020-09-03 07:20:32',
                 'updated_at' => '2020-09-03 07:20:32',
             ),
             3 =>
             array (
                 'id' => 4,
-                'name_en' => 'Corporate Trainer',
-                'name_th' => 'ประเภทสินค้า_Highway Maintenance Worker',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            4 =>
-            array (
-                'id' => 5,
-                'name_en' => 'Cafeteria Cook',
-                'name_th' => 'ประเภทสินค้า_Mechanical Engineering Technician',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            5 =>
-            array (
-                'id' => 6,
-                'name_en' => 'Forming Machine Operator',
-                'name_th' => 'ประเภทสินค้า_Solderer',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            6 =>
-            array (
-                'id' => 7,
-                'name_en' => 'Computer Operator',
-                'name_th' => 'ประเภทสินค้า_Answering Service',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            7 =>
-            array (
-                'id' => 8,
-                'name_en' => 'Maid',
-                'name_th' => 'ประเภทสินค้า_Economics Teacher',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            8 =>
-            array (
-                'id' => 9,
-                'name_en' => 'Marketing Manager',
-                'name_th' => 'ประเภทสินค้า_Installation and Repair Technician',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            9 =>
-            array (
-                'id' => 10,
-                'name_en' => 'Police Identification OR Records Officer',
-                'name_th' => 'ประเภทสินค้า_Court Reporter',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            10 =>
-            array (
-                'id' => 11,
-                'name_en' => 'Directory Assistance Operator',
-                'name_th' => 'ประเภทสินค้า_Shear Machine Set-Up Operator',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            11 =>
-            array (
-                'id' => 12,
-                'name_en' => 'Equal Opportunity Representative',
-                'name_th' => 'ประเภทสินค้า_Shuttle Car Operator',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            12 =>
-            array (
-                'id' => 13,
-                'name_en' => 'Fabric Pressers',
-                'name_th' => 'ประเภทสินค้า_Credit Checkers Clerk',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            13 =>
-            array (
-                'id' => 14,
-                'name_en' => 'Coroner',
-                'name_th' => 'ประเภทสินค้า_Human Resource Director',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            14 =>
-            array (
-                'id' => 15,
-                'name_en' => 'Radar Technician',
-                'name_th' => 'ประเภทสินค้า_Aerospace Engineer',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            15 =>
-            array (
-                'id' => 16,
-                'name_en' => 'Sound Engineering Technician',
-                'name_th' => 'ประเภทสินค้า_Painter',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            16 =>
-            array (
-                'id' => 17,
-                'name_en' => 'Aircraft Cargo Handling Supervisor',
-                'name_th' => 'ประเภทสินค้า_Camera Operator',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            17 =>
-            array (
-                'id' => 18,
-                'name_en' => 'Dental Hygienist',
-                'name_th' => 'ประเภทสินค้า_Electric Motor Repairer',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            18 =>
-            array (
-                'id' => 19,
-                'name_en' => 'Legal Secretary',
-                'name_th' => 'ประเภทสินค้า_Insulation Installer',
-                'created_at' => '2020-09-03 07:20:32',
-                'updated_at' => '2020-09-03 07:20:32',
-            ),
-            19 =>
-            array (
-                'id' => 20,
-                'name_en' => 'Streetcar Operator',
-                'name_th' => 'ประเภทสินค้า_Etcher and Engraver',
+                'name_en' => 'Drink',
+                'name_th' => 'เครื่องดื่ม',
                 'created_at' => '2020-09-03 07:20:32',
                 'updated_at' => '2020-09-03 07:20:32',
             ),
